a33caa56172f9d5f5bc2e2bcc4d82ddce25377ac
kyoungd <kyoungd@hotmail.com>
2024-06-24 15:17:40 -0700

    emailjs added and working

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -1,59 +1,74 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { toast } from 'react-toastify';
+import emailjs from 'emailjs-com';
 
 const ContactForm = () => {
+  const form = useRef();
 
-    let handleMessage = event => {
-        event.preventDefault()
-        event.target.reset()
-        toast.success("Thanks for Your Message")
-    }
+  const handleMessage = (e) => {
+    e.preventDefault();
 
-    return (
-        <>
-            <form onSubmit={handleMessage} className="contact-form">
-                <div className="row">
-                    <div className="col-lg-12">
-                        <div className="form-group">
-                            <input className="form-control" id="name" name="name" placeholder="Name*" type="text" required />
-                            <span className="alert-error"></span>
-                        </div>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-lg-6">
-                        <div className="form-group">
-                            <input className="form-control" id="email" name="email" placeholder="Email*" type="email" required />
-                            <span className="alert-error"></span>
-                        </div>
-                    </div>
-                    <div className="col-lg-6">
-                        <div className="form-group">
-                            <input className="form-control" id="phone" name="phone" placeholder="Phone" type="number" />
-                            <span className="alert-error"></span>
-                        </div>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-lg-12">
-                        <div className="form-group comments">
-                            <textarea className="form-control" id="comments" name="comments" placeholder="Tell Us About Project *" type="text" required />
-                        </div>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-lg-12">
-                        <button type="submit" name="submit" id="submit">
-                            Send Message
-                        </button>
-                    </div>
-                </div>
-                <div className="col-lg-12 alert-notification">
-                    <div id="message" className="alert-msg"></div>
-                </div>
-            </form>
-        </>
-    );
+    emailjs.sendForm(
+      process.env.REACT_APP_EMAIL_SERVICE_ID,
+      process.env.REACT_APP_EMAIL_TEMPLATE_ID,
+      form.current,
+      process.env.REACT_APP_EMAIL_USER_ID
+    ).then((result) => {
+        e.target.reset();
+        toast.success("Thanks for Your Message");
+      }, (error) => {
+        console.log(error.text);
+        e.target.reset();
+        toast.error("Failed to send email. Please try again.");
+      });
+  };
+
+  return (
+    <>
+      <form ref={form} onSubmit={handleMessage} className="contact-form">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="form-group">
+              <input className="form-control" id="user_name" name="user_name" placeholder="Name*" type="text" required />
+              <span className="alert-error"></span>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-6">
+            <div className="form-group">
+              <input className="form-control" id="user_email" name="user_email" placeholder="Email*" type="email" required />
+              <span className="alert-error"></span>
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <div className="form-group">
+              <input className="form-control" id="phone" name="phone" placeholder="Phone" type="number" />
+              <span className="alert-error"></span>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="form-group comments">
+              <textarea className="form-control" id="message" name="message" placeholder="Tell Us About Project *" type="text" required />
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-12">
+            <button type="submit" name="submit" id="submit">
+              Send Message
+            </button>
+          </div>
+        </div>
+        <div className="col-lg-12 alert-notification">
+          <input type="hidden" name="source" value="QDotData" />
+          <div id="message" className="alert-msg"></div>
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default ContactForm;
