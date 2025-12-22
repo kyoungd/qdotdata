99c065cb01e607269f35af8c806c229d0f923317
kyoungd <kyoungd@hotmail.com>
2025-12-21 22:09:33 -0800

    Netlify issue fixed.

--- src/Components/About3.js
+++ src/Components/About3.js
@@ -14,7 +14,7 @@ const About3 = () => {
                         </div>
                         <div className="about-style-six col-lg-6 offset-lg-1">
                             <h4 className="sub-title">About Us</h4>
-                            <h2 className="title">Salim Suleman; Co-founder</h2>
+                            <h2 className="title">About3 Suleman; Co-founder</h2>
                             <p>
                             Award winning telecommunications executive acknowledged internationally for launching one of the  largest satellite telecommunications networks in Africa. A true visionary with a strategic mindset and  proven performance in leading organizations through change and building cohesive teams of  talented professionals that consistently deliver exceptional growth and profits in extremely  challenging operating environments. A corporate strategy and change management specialist adept  at turning around troubled companies and motivating culturally diverse teams while maintaining core  corporate values. Led and motivated multinational team of 1,700 employees and distributors  spanning 26 countries to achieve phenomenal growth in sales and revenues topping US $ 30 million.  Vast experience spans start ups in aviation, telecommunications, oil and gas, and agriculture sectors,  including mergers and acquisitions and preparing mature businesses for sale. 
                             </p>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,8 +4,6 @@ import Header from '../Components/Header';
 import Banner from '../Components/Banner';
 import Services from '../Components/Services';
 import About from '../Components/About';
-import About2 from '../Components/About2'
-import About3 from '../Components/About3'
 import Available from '../Components/Available';
 import Blog from '../Components/Blog';
 import Contact from '../Components/Contact';
@@ -15,7 +13,6 @@ import Footer from '../Components/Footer';
 // import Pricing from '../Components/Pricing';
 // import Brands from '../Components/Brands';
 // import Testimonial from '../Components/Testimonial';
-import AiWidget from '../Components/AiWidget';
 
 const Home = () => {
     return (
